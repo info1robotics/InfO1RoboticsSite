4c23e07d2a6c3c414eb97fcfc8f11b21a5de7894
whoisplex <89403030+whoisplex@users.noreply.github.com>
2021-11-15 20:28:17 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
 /** @type {import('@sveltejs/kit').Config} */
+import node from "@sveltejs/adapter-node";
 import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
-		adapter: adapter(),
+     	adapter: node({ env: { port: process|.env.PORT } }),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
